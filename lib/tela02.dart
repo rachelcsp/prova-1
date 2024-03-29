fb4f1b5c00566e911d1fc0e25ce01cdcf5367341
Rachel <rachelcsp7@gmail.com>
2024-03-28 21:00:22 -0300

    criação tela 02

---
+++ lib/tela02.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TelaLista extends StatefulWidget {
+  const TelaLista({super.key});
+
+  @override
+  State<TelaLista> createState() => _TelaListaState();
+}
+
+class _TelaListaState extends State<TelaLista> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
